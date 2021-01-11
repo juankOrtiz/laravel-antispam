e3babe8458c22c274020876a47cd6b1f67e902d7
juankOrtiz <juank.ortiz.1991gmail.com>
2021-01-10 23:04:35 -0300

    Abstraer middleware Antispam mediante funciones

--- app/Http/Middleware/Antispam.php
+++ app/Http/Middleware/Antispam.php
@@ -9,20 +9,30 @@ class Antispam
 {
     public function handle(Request $request, Closure $next)
     {
+        // Leer valores de configuracion de Antispam
+        $config = config('antispam');
         // Comprobar si no existe el campo 'mi_nombre' en la consulta
-        if(! $request->has('mi_nombre')) {
-            abort(422, "Spam detectado");
+        if(! $request->has($config['campo_nombre'])) {
+            return $this->abortar();
         }
         // Comprobar si el campo 'mi_nombre' ha sido rellenado
-        if(! empty($request->input('mi_nombre'))) {
-            abort(422, "Spam detectado");
+        if(! empty($request->input($config['campo_nombre']))) {
+            return $this->abortar();
         }
         // Comprobar cuanto tiempo se tardo en rellenar el formulario
-        $ahora = microtime(true);
-        $tiempo_transcurrido = $ahora - $request->input('tiempo');
-        if($tiempo_transcurrido <= 3) {
-            abort(422, "Spam detectado");
+        if($this->tiempoRellenoFormulario($request) <= $config['longitud_tiempo']) {
+            return $this->abortar();
         }
         return $next($request);
+    }
+
+    // Funcion que devuelve el tiempo que se tardó en rellenar el formulario
+    protected function tiempoRellenoFormulario(Request $request) {
+        return microtime(true) - $request->input(config('antispam.campo_tiempo'));
+    }
+
+    // Funcion que arroja un error 422
+    protected function abortar() {
+        return abort(422, "Spam detectado");
     }
 }
--- config/antispam.php
+++ config/antispam.php
@@ -1,5 +1,8 @@
 <?php
 
+// Devuelve los parámetros para crear los campos el formulario oculto y definir el tiempo mínimo requerido para completar el formulario
 return [
-    
+    'campo_nombre' => 'mi_nombre',
+    'campo_tiempo' => 'tiempo',
+    'longitud_tiempo' => 3,
 ];
